c0f8e8d23bbf0a836670827505e1b6c1376f063a
zsakvo <gdwqsp@gmail.com>
2018-12-04 09:51:55 +0800

    add util to manage statusbar

--- app/src/main/java/cc/zsakvo/yueduhchelper/CacheHelperActivity.java
+++ app/src/main/java/cc/zsakvo/yueduhchelper/CacheHelperActivity.java
@@ -3,45 +3,51 @@ package cc.zsakvo.yueduhchelper;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import cc.zsakvo.yueduhchelper.utils.SnackbarUtil;
 
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.jaeger.library.StatusBarUtil;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.Permission;
 
 public class CacheHelperActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Window window = this.getWindow();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
-            if (Build.VERSION.SDK_INT<Build.VERSION_CODES.O_MR1){
-                window.setNavigationBarColor(getResources().getColor(R.color.colorPrimaryDark));
-            }
-        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cache_helper);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            StatusBarUtil.setTransparent(this);
+        }else {
+            StatusBarUtil.setColor(this,Color.parseColor("#ffffff"));
+        }
+
         toolbar = findViewById(R.id.cache_toolbar);
         toolbar.setTitle("阅读缓存提取");
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
         setSupportActionBar(toolbar);
+        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.cache_coordinatorLayout);
+        coordinatorLayout.bringToFront();
 
         if (getSharedPreferences("settings",MODE_PRIVATE).getBoolean("isFirst",true)) {
             showFirstDialog();
@@ -106,7 +112,7 @@ public class CacheHelperActivity extends AppCompatActivity {
     }
 
     public void showSnackBar(String string){
-        SnackbarUtil.build(this,toolbar,string,Snackbar.LENGTH_SHORT).show();
+        SnackbarUtil.build(this,coordinatorLayout,string,Snackbar.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/cc/zsakvo/yueduhchelper/SettingsActivity.java
+++ app/src/main/java/cc/zsakvo/yueduhchelper/SettingsActivity.java
@@ -2,12 +2,14 @@ package cc.zsakvo.yueduhchelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.content.ContextCompat;
 import cc.zsakvo.yueduhchelper.listener.ChangePathListener;
 import cc.zsakvo.yueduhchelper.utils.SnackbarUtil;
 
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
@@ -17,6 +19,7 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.jaeger.library.StatusBarUtil;
 import com.keenfin.sfcdialog.SimpleFileChooser;
 
 import java.io.File;
@@ -24,24 +27,27 @@ import java.io.File;
 public class SettingsActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Window window = this.getWindow();
+
+        setContentView(R.layout.activity_settings);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            StatusBarUtil.setTransparent(this);
+        }else {
+            StatusBarUtil.setColor(this,Color.parseColor("#ffffff"));
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            if (Build.VERSION.SDK_INT<Build.VERSION_CODES.O_MR1){
-                window.setNavigationBarColor(getResources().getColor(R.color.colorPrimaryDark));
-            }
-        }
-        setContentView(R.layout.activity_settings);
+
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         toolbar.setTitle("设置");
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
         setSupportActionBar(toolbar);
+
         if(getSupportActionBar() != null) {
             @SuppressLint("PrivateResource") Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.abc_ic_ab_back_material);
             assert upArrow != null;
@@ -55,6 +61,11 @@ public class SettingsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.coordinatorLayout);
+        coordinatorLayout.bringToFront();
+
         if (savedInstanceState==null){
             SettingsFragment fragment = new SettingsFragment();
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment,fragment).commit();
@@ -66,7 +77,7 @@ public class SettingsActivity extends AppCompatActivity {
         sfcDialog.setOnChosenListener(new SimpleFileChooser.SimpleFileChooserListener() {
             @Override
             public void onFileChosen(File file) {
-                Snackbar snackbar = SnackbarUtil.build(getApplicationContext(),toolbar,"请选择一个目录",Snackbar.LENGTH_LONG);
+                Snackbar snackbar = SnackbarUtil.build(getApplicationContext(),coordinatorLayout,"请选择一个目录",Snackbar.LENGTH_LONG);
                 snackbar.show();
             }
 
@@ -82,7 +93,7 @@ public class SettingsActivity extends AppCompatActivity {
 
             @Override
             public void onCancel() {
-                Snackbar snackbar = SnackbarUtil.build(getApplicationContext(),toolbar,"未选择任何目录",Snackbar.LENGTH_LONG);
+                Snackbar snackbar = SnackbarUtil.build(getApplicationContext(),coordinatorLayout,"未选择任何目录",Snackbar.LENGTH_LONG);
                 snackbar.show();
             }
         });
